396275e4f9c51148fbc8111f81293e8519b7022c
odasaburo <odasaburoo@gmail.com>
2015-12-01 21:27:55 +0000

    Gussa
    Created UI

---
+++ Gussa/Assets/Scripts/splash.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class splash : MonoBehaviour {
+
+    public float count = 0;
+	// Use this for initialization
+	void Start () {
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        count += Time.deltaTime;
+        if(count >= 4f)
+        {
+            Application.LoadLevel("menu");
+        }
+	}
+}
