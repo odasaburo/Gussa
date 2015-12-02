62429c1cefc4743722f9b89b895ce6f84daa856c
odasaburo <odasaburoo@gmail.com>
2015-12-02 21:38:59 +0000

    Gussa
    Created Main screen.

---
+++ Gussa/Assets/Scripts/ImageTransparency.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageTransparency : MonoBehaviour {
+
+    public Color color;
+    public float value;
+	// Use this for initialization
+	void Start () {
+        color = this.gameObject.GetComponent<Image>().color;
+        color.a = value;
+        GetComponent<Image>().color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetTransparency(float transparency)
+    {
+        color.a = transparency;
+        GetComponent<Image>().color = color;
+    }
+}
