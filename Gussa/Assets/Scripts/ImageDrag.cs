7ac068eab36a4084de3f92fe897d768824d3d15b
odasaburo <odasaburoo@gmail.com>
2015-12-10 02:24:37 +0000

    Gussa
    completed scene 1

---
+++ Gussa/Assets/Scripts/ImageDrag.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImageDrag : MonoBehaviour {
+
+    public Vector2 offset;
+    public GameObject whiteobject;
+    public bool dragState;
+    public bool upstate;
+    public Vector2 oldpos;
+    public string gamelevelName;
+    public string animationstate;
+    // Use this for initialization
+    void Start () {
+         oldpos = GetComponent<Transform>().position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void DownEvent()
+    {
+        if (dragState)
+        {
+            GetComponent<Transform>().position = new Vector3( Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public void UpEvent()
+    {
+        if (upstate)
+        {
+            whiteobject = GameObject.Find(this.gameObject.name.ToString() + "1");
+            if (GetComponent<Transform>().position.x + offset.x > whiteobject.GetComponent<Transform>().position.x &&
+               GetComponent<Transform>().position.x - offset.x < whiteobject.GetComponent<Transform>().position.x &&
+               GetComponent<Transform>().position.y + offset.y > whiteobject.GetComponent<Transform>().position.y &&
+               GetComponent<Transform>().position.y - offset.y < whiteobject.GetComponent<Transform>().position.y)
+            {
+                whiteobject.GetComponent<Image>().sprite = GetComponent<Image>().sprite;
+                whiteobject.GetComponent<ImageDrag>().dragState = true;
+                Destroy(this.gameObject);
+            }
+            else {
+                GetComponent<Transform>().position = oldpos;
+            }
+        }
+        else if (GameObject.Find(gamelevelName).GetComponent<LevelManager>().GameAnimationstart)
+        {
+            animationstate = "putdough";
+            GameObject.Find(gamelevelName).GetComponent<LevelManager>().PlayAnimations(animationstate);
+        }
+        else
+        {
+            GetComponent<Transform>().position = oldpos;
+        }
+    }
+}
---
+++ Gussa/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject[] founds_object;
+    public GameObject[] checkstate;
+    public GameObject[] level_animations;
+    public int gamelevel;
+    public bool GameAnimationstart;
+	// Use this for initialization
+	void Start () {
+        GameAnimationstart = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int count = 0;
+        for (int i = 0; i < founds_object.Length; i++)
+        {
+            if(founds_object[i] == null)
+            {
+                count++;
+            }
+        }
+
+        if(count == founds_object.Length)
+        {
+            Debug.Log("start");
+            GameAnimationstart = true;
+        }
+
+        int complete = 0;
+
+        for(int i = 0; i < checkstate.Length; i++)
+        {
+            if (checkstate[i].activeSelf)
+            {
+                complete++;
+            }
+        }
+        if (complete == checkstate.Length)
+        {
+            Debug.Log("game complete");
+        }
+	}
+
+    public void PlayAnimations(string state)
+    {
+        level_animations[gamelevel].SetActive(true);
+        level_animations[gamelevel].GetComponent<Animator>().SetTrigger(state);
+    }
+}
